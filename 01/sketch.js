d1a5d3929dfa0e227629afb4d50a46b42b569eb0
Zach Krall <zach@zachkrall.com>
2018-09-03 22:40:09 -0400

    01

--- 01/sketch.js
+++ 01/sketch.js
@@ -1,21 +1,29 @@
-var myRec = new p5.SpeechRec(); // new P5.SpeechRec object
+var myRec = new p5.SpeechRec();
 myRec.continuous = true;
+
 var myString = '';
 var buffer = 20;
 
-	function setup()
-	{
-		// graphics stuff:
-		createCanvas(windowWidth, windowHeight);
+function clr(){
 
-    textSize(50);
-    textFont('Menlo');
-		// instructions:
-		myRec.onResult = showResult;
-		myRec.start();
-	}
-	function draw()
-	{
+	myString = '';
+	console.log( 'clr()' );
+
+}
+
+function setup(){
+
+	createCanvas(windowWidth, windowHeight);
+
+	textSize(50);
+	textFont('Menlo');
+
+	myRec.onResult = showResult;
+	myRec.start();
+
+}
+
+function draw(){
 
     background(0, 0, 255);
 
@@ -29,19 +37,22 @@ var buffer = 20;
 
     textAlign(CENTER, CENTER);
     text(myString.toUpperCase(), 0+buffer, 0+buffer, width-buffer*2, height-buffer*2);
-	}
-	function showResult()
-	{
+
+}
+
+function showResult(){
+
     if(myRec.resultString == 'clear'){
-      myString = '';
+	    clr();
     } else if(myRec.resultValue==true) {
       myString = myString + ' ' + myRec.resultString;
 			console.log(myRec.resultString);
 		}
-	}
 
-  function windowResized(){
+}
 
-    resizeCanvas(windowWidth,windowHeight);
+function windowResized(){
 
-  }
+	resizeCanvas(windowWidth,windowHeight);
+
+}
